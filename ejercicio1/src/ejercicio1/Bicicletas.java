f23f63e9b2ed9845b2a809cb15652e2bd957d2bd
rafa <rafakun123@gmail.com>
2022-01-13 00:18:20 +0100

    actualizado con ejercicio2

--- ejercicio1/src/ejercicio1/Bicicletas.java
+++ ejercicio1/src/ejercicio1/Bicicletas.java
@@ -1,15 +1,40 @@
 package ejercicio1;
-
+import java.util.Scanner;
 public class Bicicletas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
 		bicicleta mibicicleta = new bicicleta(20,2,4);
 		bicicleta tubicicleta = new bicicleta(15,1,5);
 		
-		System.out.print("La velocidad de mi bici despues de acelerar es: ");System.out.println(mibicicleta.acelerar());
-		System.out.print("La velocidad de tu bici despues de acelerar es: ");System.out.println(tubicicleta.acelerar());
-		System.out.print("La velocidad de mi bici despues de frenar es: ");System.out.println(mibicicleta.frenar());
-		System.out.print("La velocidad de tu bici despues de frenar es: ");System.out.println(mibicicleta.frenar());
-}
+		System.out.println("ELIGE ENTRE LAS SIGUENTES OPCIONES: (1)Acelerar las dos bicicletas, (2)Frenar las dos bicicletas, (3)Cambiar el plato y cambiar el piñon, (4)Valores por defecto para plato y piñon.");
+		int num = in.nextInt();
+		switch (num) 
+		{
+		case 1:
+			System.out.print("La velocidad de mi bici despues de acelerar es: ");System.out.println(mibicicleta.acelerar());
+			System.out.print("La velocidad de tu bici despues de acelerar es: ");System.out.println(tubicicleta.acelerar());
+		case 2:
+			System.out.print("La velocidad de mi bici despues de frenar es: ");System.out.println(mibicicleta.frenar());
+			System.out.print("La velocidad de tu bici despues de frenar es: ");System.out.println(tubicicleta.frenar());
+		case 3:
+			System.out.println("De que bicicleta deseas cambiar el plato: 1)Mi bicibleta 2)Tu bicicleta");
+			int num1 = in.nextInt();
+			if (num1 == 1) {
+				System.out.println(mibicicleta.cambiarplato(num));
+				System.out.println(mibicicleta.cambiarpinion(num));
+			}else {
+				System.out.println(tubicicleta.cambiarplato(num));
+				System.out.println(tubicicleta.cambiarpinion(num));
+			}
+		case 4:
+			System.out.println(mibicicleta.platoypinionpordefecto());
+		}
+		
+		
+		
+
+	
+	}
 }
--- ejercicio1/src/ejercicio1/bicicleta.java
+++ ejercicio1/src/ejercicio1/bicicleta.java
@@ -1,6 +1,8 @@
 package ejercicio1;
+import java.util.Scanner;
 
 public class bicicleta {
+	Scanner in = new Scanner(System.in);
 
 	private int velocidad;
 	private int plato;
@@ -22,7 +24,24 @@ public class bicicleta {
 		velocidad = velocidad/2;
 		return velocidad;
 	}
-	public void cambiarplato(int plato) {}
-	public void cambiarpinion(int pinion) {}
+	public int cambiarplato(int plato) {
+		System.out.println("Selecciona el plato deseado entre 1, 2, 3.");
+		int num = in.nextInt();
+		plato = num;
+		System.out.print("El plato seleccionado es ");return plato;
+	}
+	public int cambiarpinion(int pinion) {
+		System.out.println("Selecciona el piñon deseado entre 1 y 6");
+		int num = in.nextInt();
+		pinion = num;
+		System.out.print("El plato seleccionado es ");return pinion;
+	}
 
+	public int platoypinionpordefecto() {
+		plato = 1;
+		pinion = 1;
+		System.out.println("Ahora el plato y piñon de ambas bicicletas estan por defecto, en concreto");
+			return pinion;
+	}
+	
 }
